d8790f3b8d3e3f84af559decac88f82eb2ca8f84
Dylan Baker <38574400+dbvker@users.noreply.github.com>
2021-11-03 13:22:40 -0500

    Update Account.js

--- src/views/Account.js
+++ src/views/Account.js
@@ -4,7 +4,6 @@ import React from "react";
 // Components
 import AccountNavigation from "../components/account/accountNavigation/AccountNavigation";
 import RewardsTile from "../components/account/rewardsTile/RewardsTile";
-import RewardsTile from "../components/account/rewardsTile/RewardsTile";
 
 const Account = () => {
   return (
