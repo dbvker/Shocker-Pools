b8fec30ba911735d62905434119668e0bd485e75
Dylan Baker <dylanmbaker@yahoo.com>
2021-11-10 12:24:05 -0600

    added a headerAlert and designed the welcomecard

--- src/components/welcomeCard/WelcomeCard.js
+++ src/components/welcomeCard/WelcomeCard.js
@@ -1,14 +1,29 @@
 // React
 import React from "react";
-import { Link } from "react-router-dom";
 
 //Import CSS
 import "./WelcomeCard.css";
 
 const WelcomeCard = () => {
   return (
-    <div className="welcome_wrapper">
-    </div>
+    <section className="welcome-container">
+      <div className="welcome-container-left">
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+        <h1>Enjoy your pool this summer. Let us do the work.</h1>
+        <h3>
+          Our team offers the highest quality pool care so you can enjoy your
+          summer without having to worry about your pool once.
+        </h3>
+        <button>Get your first cleaning free</button>
+        <span>Our Services</span>
+      </div>
+      <div className="welcome-container-right">
+        <div className="circle3"></div>
+        <div className="circle4"></div>
+        <div className='welcome-container-right-image'></div>
+      </div>
+    </section>
   );
 };
 
